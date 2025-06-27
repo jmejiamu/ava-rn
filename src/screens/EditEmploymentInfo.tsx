47906a54a38dc17cf1a1967cf841ebbc6f3eb4e2
jmejiamu <jmejiamu@gmail.com>
2025-06-27 16:37:56 -0700

    fix(employment): employment edit screens

--- src/screens/EditEmploymentInfo.tsx
+++ src/screens/EditEmploymentInfo.tsx
@@ -21,7 +21,11 @@ import { FontSizes } from "@/theme/fontSizes";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { formatDate } from "@/utils/formatDate";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Calendar from "../../assets/icons/Calendar.svg";
+import YesRadioButton from "../../assets/icons/YesRadioButton.svg";
+import NoRadioBtn from "../../assets/icons/NoRadioBtn.svg";
+import { ActionButton } from "@/components/ActionButton";
+import isEqual from "lodash.isequal";
 
 const EditEmploymentInfo = () => {
   const navigation = useNavigation();
@@ -42,9 +46,22 @@ const EditEmploymentInfo = () => {
   });
 
   const [form, setForm] = useState<EmploymentInfo | null>(null);
+  const [initialForm, setInitialForm] = useState<EmploymentInfo | null>(null);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [payFreqOpen, setPayFreqOpen] = useState(false);
   const [showDatePicker, setShowDatePicker] = useState(false);
+  const [yearsOpen, setYearsOpen] = useState(false);
+  const [monthsOpen, setMonthsOpen] = useState(false);
+  const [selectedYear, setSelectedYear] = useState<number | null>(
+    data?.timeWithEmployer
+      ? parseInt(data.timeWithEmployer.split(" ")[0])
+      : null
+  );
+  const [selectedMonth, setSelectedMonth] = useState<number | null>(
+    data?.timeWithEmployer
+      ? parseInt(data.timeWithEmployer.split(" ")[2])
+      : null
+  );
   const [dropdownItems] = useState([
     { label: "Full-time", value: "Full-time" },
     { label: "Part-time", value: "Part-time" },
@@ -59,9 +76,24 @@ const EditEmploymentInfo = () => {
     { label: "Monthly", value: "Monthly" },
     { label: "Other", value: "Other" },
   ]);
+  const [years, setYears] = useState(
+    Array.from({ length: 31 }, (_, i) => ({
+      label: `${i} year${i === 1 ? "" : "s"}`,
+      value: i,
+    }))
+  );
+  const [months, setMonths] = useState(
+    Array.from({ length: 12 }, (_, i) => ({
+      label: `${i + 1} month${i + 1 === 1 ? "" : "s"}`,
+      value: i + 1,
+    }))
+  );
 
   useEffect(() => {
-    if (data) setForm(data);
+    if (data) {
+      setForm(data);
+      setInitialForm(data);
+    }
   }, [data]);
 
   if (isLoading || !form) return <ActivityIndicator style={{ flex: 1 }} />;
@@ -74,6 +106,8 @@ const EditEmploymentInfo = () => {
   const handleSave = () => {
     if (form) mutation.mutate(form);
   };
+
+  const isFormChanged = !isEqual(form, initialForm);
 
   return (
     <SafeAreaView
@@ -159,11 +193,7 @@ const EditEmploymentInfo = () => {
               width: 44,
             }}
           >
-            <FontAwesome5
-              name="calendar-week"
-              size={20}
-              color={AppTheme.colors.ava_text_primary_light}
-            />
+            <Calendar width={20} height={20} />
           </TouchableOpacity>
         </View>
         <DateTimePickerModal
@@ -188,31 +218,125 @@ const EditEmploymentInfo = () => {
             flexDirection: "row",
             alignItems: "center",
             marginBottom: 16,
+            marginTop: 24,
           }}
         >
-          <Button
-            title={form.isDirectDeposit ? "Yes" : "No"}
-            onPress={() =>
-              handleChange("isDirectDeposit", !form.isDirectDeposit)
-            }
-          />
+          <TouchableOpacity
+            onPress={() => handleChange("isDirectDeposit", true)}
+            style={{ flexDirection: "row", alignItems: "center" }}
+            activeOpacity={0.7}
+          >
+            {form.isDirectDeposit ? (
+              <YesRadioButton width={24} height={24} />
+            ) : (
+              <NoRadioBtn width={24} height={24} />
+            )}
+            <Text
+              style={{
+                marginLeft: 4,
+                color: AppTheme.colors.ava_text_primary_dark,
+                fontSize: FontSizes.md,
+              }}
+            >
+              Yes
+            </Text>
+          </TouchableOpacity>
+          <View style={{ marginHorizontal: 50 }} />
+          <TouchableOpacity
+            onPress={() => handleChange("isDirectDeposit", false)}
+            style={{ flexDirection: "row", alignItems: "center" }}
+            activeOpacity={0.7}
+          >
+            {!form.isDirectDeposit ? (
+              <YesRadioButton width={24} height={24} />
+            ) : (
+              <NoRadioBtn width={24} height={24} />
+            )}
+            <Text
+              style={{
+                marginLeft: 4,
+                color: AppTheme.colors.ava_text_primary_dark,
+                fontSize: FontSizes.md,
+              }}
+            >
+              No
+            </Text>
+          </TouchableOpacity>
         </View>
         <Text style={styles.label}>Employer address</Text>
         <TextInput
           style={styles.input}
           value={form.employerAddress}
+          multiline
+          numberOfLines={3}
           onChangeText={(v) => handleChange("employerAddress", v)}
         />
-        <Text style={styles.label}>Time with employer</Text>
-        <TextInput
-          style={styles.input}
-          value={form.timeWithEmployer}
-          onChangeText={(v) => handleChange("timeWithEmployer", v)}
-        />
-        <Button
-          title="Save"
+        <Text
+          style={[
+            styles.label,
+            {
+              fontSize: FontSizes.xs,
+              fontWeight: "400",
+              color: AppTheme.colors.ava_text_primary_light,
+            },
+          ]}
+        >
+          Time with employer
+        </Text>
+        <View style={{ flexDirection: "row", gap: 8, marginBottom: 8 }}>
+          <View style={{ flex: 1 }}>
+            <DropDownPicker
+              open={yearsOpen}
+              value={selectedYear}
+              items={years}
+              setOpen={setYearsOpen}
+              setValue={(cb) => {
+                const val = cb(selectedYear);
+                setSelectedYear(val);
+                handleChange(
+                  "timeWithEmployer",
+                  `${val} year${val === 1 ? "" : "s"} ${selectedMonth} month${
+                    selectedMonth === 1 ? "" : "s"
+                  }`
+                );
+              }}
+              setItems={setYears}
+              placeholder="Years"
+              containerStyle={{ marginBottom: 0 }}
+              style={{ borderColor: "#ccc", borderRadius: 8, padding: 16 }}
+              dropDownContainerStyle={{ borderColor: "#ccc" }}
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            <DropDownPicker
+              open={monthsOpen}
+              value={selectedMonth}
+              items={months}
+              setOpen={setMonthsOpen}
+              setValue={(cb) => {
+                const val = cb(selectedMonth);
+                setSelectedMonth(val);
+                handleChange(
+                  "timeWithEmployer",
+                  `${selectedYear} year${
+                    selectedYear === 1 ? "" : "s"
+                  } ${val} month${val === 1 ? "" : "s"}`
+                );
+              }}
+              setItems={setMonths}
+              placeholder="Months"
+              containerStyle={{ marginBottom: 0 }}
+              style={{ borderColor: "#ccc", borderRadius: 8, padding: 16 }}
+              dropDownContainerStyle={{ borderColor: "#ccc" }}
+            />
+          </View>
+        </View>
+
+        <ActionButton
+          title="Continue"
           onPress={handleSave}
-          disabled={mutation.isPending}
+          disabled={mutation.isPending || !isFormChanged}
+          style={{ marginTop: 16 }}
         />
       </ScrollView>
     </SafeAreaView>
